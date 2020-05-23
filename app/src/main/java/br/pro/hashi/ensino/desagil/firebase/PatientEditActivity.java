7bf8505ca16867421b18f7a732c65880ac71b7d8
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-05-23 18:27:49 -0300

    add finalizar button

--- app/src/main/java/br/pro/hashi/ensino/desagil/firebase/PatientEditActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/firebase/PatientEditActivity.java
@@ -1,7 +1,9 @@
 package br.pro.hashi.ensino.desagil.firebase;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,8 +19,8 @@ public class PatientEditActivity extends AppCompatActivity {
     private String patientName;
     private String listaSintomas[] = {"Tosse", "Febre", "Coriza", "Dor de cabeça", "Dor no corpo"};
     private List<Comorbidade> listaComorbidadesEnum = Arrays.asList(Comorbidade.class.getEnumConstants());
-
-    private List<String> sintomasSelecionados;
+    private Button finalizarButton;
+    private List<String> comorbidadesSelecionadas, sintomasSelecionados;
     ArrayAdapter<String> adapterSintomas, adapterComorbidades;
 
 
@@ -30,7 +32,6 @@ public class PatientEditActivity extends AppCompatActivity {
            listaComorbidades.add(e.getNomeComorbidades());
             //System.out.println(e.getNomeComorbidades());
         }
-        System.out.println(patientName);
 
 
         super.onCreate(savedInstanceState);
@@ -42,13 +43,40 @@ public class PatientEditActivity extends AppCompatActivity {
         patientIdadeEdit = findViewById(R.id.edit_idade);
         listaSintomasView = findViewById(R.id.list_sintomas);
         listaComorbidadesView = findViewById(R.id.list_comorbidades);
+        finalizarButton = findViewById((R.id.finalizar));
         patientName = patientNameEdit.getText().toString();
         adapterSintomas = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, listaSintomas);
         adapterComorbidades = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, listaComorbidades);
         listaSintomasView.setAdapter(adapterSintomas);
         listaComorbidadesView.setAdapter(adapterComorbidades);
 
+        finalizarButton.setOnClickListener((view) -> {
+            switch(view.getId()){
+                case R.id.finalizar:
+                    sintomasSelecionados = new ArrayList<>();
+                    comorbidadesSelecionadas = new ArrayList<>();
+                    SparseBooleanArray sintomasChecked = listaSintomasView.getCheckedItemPositions();
+                    SparseBooleanArray comorbidadesChecked = listaSintomasView.getCheckedItemPositions();
+                    for(int i = 0;i<sintomasChecked.size(); i++){
+                        int key =  sintomasChecked.keyAt(i);
+                        boolean value = sintomasChecked.get(key);
+                        if(value){
+                            sintomasSelecionados.add(listaSintomasView.getItemAtPosition(key).toString());
+                        }
+                    }
+                    for(int i = 0;i<comorbidadesChecked.size(); i++){
+                        int key =  comorbidadesChecked.keyAt(i);
+                        boolean value = comorbidadesChecked.get(key);
+                        if(value){
+                            comorbidadesSelecionadas.add(listaComorbidadesView.getItemAtPosition(key).toString());
+                        }
+                    }
+            }
+            System.out.println(sintomasSelecionados);
+            System.out.println(comorbidadesSelecionadas);
 
+        });
 
     }
+
 }
